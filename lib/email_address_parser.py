d6a510be2635eecad8c0ced54bf2a6044819820d
sup56006 <isaackyuma56@gmail.com>
2025-11-21 23:41:46 +0300

    Add email-parser

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, emails):
+        self.emails = emails
+
+    def parse(self):
+        # Find ONLY valid email patterns
+        found_emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}", self.emails)
+
+        # Return sorted unique emails
+        return sorted(set(found_emails))
